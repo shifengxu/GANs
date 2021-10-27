3cff087061a906a439f06975a95b0f43411eb55b
CSLS34\shifeng <shifeng001@e.ntu.edu.sg>
2021-10-27 23:02:51 +0800

    In file fc_gan.py, add functions: main(), run_epoch_batch(). in .gitignore file, append .idea/ folder.

--- SimpleGAN/fc_gan.py
+++ SimpleGAN/fc_gan.py
@@ -1,3 +1,5 @@
+import datetime
+import time
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -37,71 +39,85 @@ class Generator(nn.Module):
 
 
 # Hyperparameters etc.
-device = "cuda" if torch.cuda.is_available() else "cpu"
+gpu_ids = [2, 3]
+device = f"cuda:{gpu_ids[0]}" if torch.cuda.is_available() else "cpu"
 lr = 3e-4
 z_dim = 64
 image_dim = 28 * 28 * 1  # 784
-batch_size = 32
-num_epochs = 50
+batch_size = 1024
+num_epochs = 100
 
-disc = Discriminator(image_dim).to(device)
-gen = Generator(z_dim, image_dim).to(device)
-fixed_noise = torch.randn((batch_size, z_dim)).to(device)
-transforms = transforms.Compose(
-    [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,)),]
-)
+disc = Discriminator(image_dim)
+genr = Generator(z_dim, image_dim)
+fino = torch.randn((batch_size, z_dim))  # fixed noise
+if len(gpu_ids) > 1:
+    disc = torch.nn.DataParallel(disc, device_ids=gpu_ids)
+    genr = torch.nn.DataParallel(genr, device_ids=gpu_ids)
+disc = disc.to(device)
+genr = genr.to(device)
+fino = fino.to(device)
+transforms = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,)),  # (mean, std, inplace=False)
+])
 
 dataset = datasets.MNIST(root="dataset/", transform=transforms, download=True)
-loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
 opt_disc = optim.Adam(disc.parameters(), lr=lr)
-opt_gen = optim.Adam(gen.parameters(), lr=lr)
+opt_genr = optim.Adam(genr.parameters(), lr=lr)
 criterion = nn.BCELoss()
 writer_fake = SummaryWriter(f"logs/fake")
 writer_real = SummaryWriter(f"logs/real")
-step = 0
 
-for epoch in range(num_epochs):
-    for batch_idx, (real, _) in enumerate(loader):
-        real = real.view(-1, 784).to(device)
-        batch_size = real.shape[0]
 
-        ### Train Discriminator: max log(D(x)) + log(1 - D(G(z)))
-        noise = torch.randn(batch_size, z_dim).to(device)
-        fake = gen(noise)
-        disc_real = disc(real).view(-1)
-        lossD_real = criterion(disc_real, torch.ones_like(disc_real))
-        disc_fake = disc(fake).view(-1)
-        lossD_fake = criterion(disc_fake, torch.zeros_like(disc_fake))
-        lossD = (lossD_real + lossD_fake) / 2
-        disc.zero_grad()
-        lossD.backward(retain_graph=True)
-        opt_disc.step()
+# run the training process for a single epoch and a single batch
+def run_epoch_batch(epoch, batch_idx, real):
+    # real shape is [32, 1, 28, 28] and 32 is the batch size.
+    # torch.tensor.view: return a new tensor with the same data but of a different shape.
+    # the size -1 is inferred from other dimensions
+    real = real.view(-1, 784).to(device)
 
-        ### Train Generator: min log(1 - D(G(z))) <-> max log(D(G(z))
-        # where the second option of maximizing doesn't suffer from
-        # saturating gradients
-        output = disc(fake).view(-1)
-        lossG = criterion(output, torch.ones_like(output))
-        gen.zero_grad()
-        lossG.backward()
-        opt_gen.step()
+    # Train Discriminator: max log(D(x)) + log(1 - D(G(z)))
+    noise = torch.randn(real.shape[0], z_dim).to(device)  # real.shape[0] is batch size
+    fake = genr(noise)
+    disc_real = disc(real).view(-1)
+    disc_fake = disc(fake).view(-1)
+    lossD_real = criterion(disc_real, torch.ones_like(disc_real))
+    lossD_fake = criterion(disc_fake, torch.zeros_like(disc_fake))
+    lossD = (lossD_real + lossD_fake) / 2
+    disc.zero_grad()
+    lossD.backward(retain_graph=True)
+    opt_disc.step()
 
-        if batch_idx == 0:
-            print(
-                f"Epoch [{epoch}/{num_epochs}] Batch {batch_idx}/{len(loader)} \
-                      Loss D: {lossD:.4f}, loss G: {lossG:.4f}"
-            )
+    # Train Generator: min log(1 - D(G(z))) <-> max log(D(G(z)))
+    # where the second option of maximizing doesn't suffer from saturating gradients
+    output = disc(fake).view(-1)
+    lossG = criterion(output, torch.ones_like(output))
+    genr.zero_grad()
+    lossG.backward()
+    opt_genr.step()
 
-            with torch.no_grad():
-                fake = gen(fixed_noise).reshape(-1, 1, 28, 28)
-                data = real.reshape(-1, 1, 28, 28)
-                img_grid_fake = torchvision.utils.make_grid(fake, normalize=True)
-                img_grid_real = torchvision.utils.make_grid(data, normalize=True)
+    if batch_idx == 0:
+        now = time.time() + 60 * 60 * 8  # seconds. change timezone to UTC+08:00
+        dtstr = datetime.datetime.utcfromtimestamp(now).strftime("%Y-%m-%d %H:%M:%S")
+        print(f"{dtstr} E[{epoch}/{num_epochs}] B[{batch_idx}] LossD:{lossD:.4f} lossG:{lossG:.4f}")
+        with torch.no_grad():
+            fake = genr(fino).reshape(-1, 1, 28, 28)
+            data = real.reshape(-1, 1, 28, 28)
+            img_grid_fake = torchvision.utils.make_grid(fake, normalize=True)
+            img_grid_real = torchvision.utils.make_grid(data, normalize=True)
+            writer_fake.add_image("Mnist Fake Images", img_grid_fake, global_step=epoch)
+            writer_real.add_image("Mnist Real Images", img_grid_real, global_step=epoch)
+        # with
+    # if
 
-                writer_fake.add_image(
-                    "Mnist Fake Images", img_grid_fake, global_step=step
-                )
-                writer_real.add_image(
-                    "Mnist Real Images", img_grid_real, global_step=step
-                )
-                step += 1
+
+def main():
+    for epoch in range(num_epochs):
+        for batch_idx, (real, _) in enumerate(loader):
+            run_epoch_batch(epoch, batch_idx, real)
+    # for
+
+
+if __name__ == "__main__":
+    main()
